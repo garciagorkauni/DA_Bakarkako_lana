daa29663a0fee033b3150df7208e857004ab346f
Gorka Garcia <114485898+garciagorkauni@users.noreply.github.com>
2024-01-06 19:38:06 +0100

    Developed basic repository methods

--- nba-restapi/src/main/java/dambi/nbarestapi/model/GameRepository.java
+++ nba-restapi/src/main/java/dambi/nbarestapi/model/GameRepository.java
@@ -1,5 +1,20 @@
 package dambi.nbarestapi.model;
 
-public class GameRepository {
-    
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository {
+    // Jolas guztien datuak ikusteko metodoa
+    List<Game> findAllGames(String gameId);
+
+    // Jolas baten datuak ikusteko metodoa bere ID-a erabiliz
+    Game findByGameId(String gameId);
+
+    // Jolas bat kentzeko metodoa bere ID-a erabiliz
+    void deleteByGameId(String gameId);
+
+    // Jolas baten emaitzak aldatzeko metodoa bere ID-a erabiliz
+    void updatePointsByGameId(String gameId, int ptsHome, int ptsVisitor);
 }
--- nba-restapi/src/main/java/dambi/nbarestapi/model/MongoGameRepository.java
+++ nba-restapi/src/main/java/dambi/nbarestapi/model/MongoGameRepository.java
@@ -1,5 +1,62 @@
 package dambi.nbarestapi.model;
 
-public class MongoGameRepository {
-    
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mongodb.ReadConcern;
+import com.mongodb.ReadPreference;
+import com.mongodb.TransactionOptions;
+import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+
+
+import static com.mongodb.client.model.Filters.eq;
+
+@Repository
+public class MongoGameRepository implements GameRepository {
+
+    private static final TransactionOptions txnOptions = TransactionOptions.builder()
+                                                                        .readPreference(ReadPreference.primary())
+                                                                        .readConcern(ReadConcern.MAJORITY)
+                                                                        .writeConcern(WriteConcern.MAJORITY)
+                                                                        .build();
+    @Autowired
+    private MongoClient client;
+    private MongoCollection<Game> gamesCollection;
+
+    // Methodo honek, dependentziak injektatu ostean exekutatuko da, eta kolekzioarekin konexioa ezarriko du.
+    @PostConstruct
+    void init() {
+        gamesCollection = client.getDatabase("nba").getCollection("games", Game.class);
+    }
+
+    // Jolas guztien datuak ikusteko metodoa
+    @Override
+    public List<Game> findAllGames(String gameId) {
+        return gamesCollection.find().into(new ArrayList<>());
+    }
+
+    // Jolas baten datuak ikusteko metodoa bere ID-a erabiliz
+    @Override
+    public Game findByGameId(String gameId) {
+        return gamesCollection.find(eq("game_id", new ObjectId(gameId))).first();
+    }
+
+    // Jolas bat kentzeko metodoa bere ID-a erabiliz
+    @Override
+    public void deleteByGameId(String gameId) {
+        gamesCollection.deleteOne(eq("game_id", new ObjectId(gameId)));        
+    }
+
+    // Jolas baten emaitzak aldatzeko metodoa bere ID-a erabiliz
+    @Override
+    public void updatePointsByGameId(String gameId, int ptsHome, int ptsVisitor) {
+        throw new UnsupportedOperationException("Unimplemented method 'updatePointsByGameId'");
+    }
 }
